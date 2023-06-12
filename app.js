e83de93d6e7587cac210ae699186e8f5cca2b031
aleksandr9707 <alex9707007@gmail.com>
2023-06-12 02:01:34 -0400

    new commit

--- app.js
+++ app.js
@@ -1,12 +1,44 @@
-//  const $btn = $('<button>')
-//  $btn.on('click',)
-
- $('#toggleButton').click(function() {
-    $('#descriptionBox').toggle();
-});
 
 $(document).ready(function() {
-    $(".projectdiv").each(function(index) {
-        $(this).delay(500 * index).animate({ opacity: 1 }, 1000); // Adjust the duration and delay as desired
+    $('#button').click(function() {
+        $('#descriptionBox').slideToggle();
       });
+
+      var imageIndex = 0;
+      var imageUrls = [
+          "https://i.imgur.com/wMds5Jx.jpg",
+          "https://i.imgur.com/oGeh27M.jpg",
+          "https://i.imgur.com/croIVsG.jpg" // Add more image URLs here
+        
+      ];
+  
+      $("#profilepic").click(function() {
+          imageIndex = (imageIndex + 1) % imageUrls.length;
+          $(this).attr("src", imageUrls[imageIndex]);
+      });
+
+    $(".projectdiv").each(function(index) {
+        $(this).delay(400 * index).animate({ opacity: 1 }, 100); // Adjust the duration and delay as desired
+      });
+    
+    const $linkedinButton = $('<button id="linkedin">LinkedIn</button>');
+
+      // Add click event handler to the button
+      $linkedinButton.click(function() {
+          window.location.href = 'https://www.linkedin.com/in/aleksandr-samedinov-77a63227a/'
+      });
+
+      const $githubButton = $('<button id="github">GitHub</button>');
+
+      // Add click event handler to the button
+      $githubButton.click(function() {
+        window.location.href = 'https://github.com/aleksandr9707'
+      });
+
+
+      $('#buttonContainer').append($linkedinButton);
+      $('#buttonContainer').append($githubButton);
   });
+
+
+  
